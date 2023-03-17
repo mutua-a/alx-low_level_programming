66e647a6ad3287b7ce52502bb63404651277bfd2
mutua-a <mnzyokaeric@gmail.com>
2023-03-17 05:58:51 -0700

    put, function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main 
- * return: 0
+ * main - main block
+ * Return: 0
  */
-
-int main(void){
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
