2472627c329eacb6a9bc6568ea9961bb26c6bb6f
mutua-a <mnzyokaeric@gmail.com>
2023-03-21 04:06:28 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,20 @@
-#inlude<stdio.h>
-int main(){
-  putchar('\n');
-  return o;
+#include "main.h"
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	char holberton[] = "_putchar";
+	int i = 0;
+
+	while (holberton[i] != '\0')
+	{
+		_putchar(holberton[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
